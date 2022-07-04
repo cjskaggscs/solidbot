14d97a4478aaed4853b87125d0a018756a0533af
cjskaggs <cameronskaggscs@gmail.com>
2022-07-04 12:22:53 -0700

    Dice rolling added

--- src/Modules/CommonModules.cs
+++ src/Modules/CommonModules.cs
@@ -2,6 +2,7 @@
 using Discord.WebSocket;
 using System;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Discord_Bot
@@ -38,6 +39,50 @@ namespace Discord_Bot
 
             // ReplyAsync() is a shortcut for Context.Channel.SendMessageAsync()
             await ReplyAsync($"I choose **{selection.Trim()}**.");
+        }
+
+        [Command("roll")]
+        [Summary("Roll the dice.")]
+        public async Task Roll([Remainder]string message = "")
+        {
+            Match stringMatch = Regex.Match(message, @"\b(?<numDice>\d*)(?i)d(?<valueDice>\d*)$");
+
+            try
+            {
+                int numDice = int.Parse(stringMatch.Groups["numDice"].Value);
+                int valueDice = int.Parse(stringMatch.Groups["valueDice"].Value);
+
+                if (numDice < 1 || valueDice < 1)
+                    throw new ArgumentOutOfRangeException($"numDice: {numDice}\t valueDice: {valueDice}");
+
+                int result = 0;
+                Random random = new Random();
+
+                for (int i = 0; i < numDice; i++)
+                {
+                    result += random.Next(1, valueDice + 1);
+                }
+
+                await ReplyAsync($"**{result}**");
+            }
+            catch (FormatException ex)
+            {
+                await ReplyAsync($"{ex.Message} Please use the format ;roll `<num1>`d`<num2>`.");
+                Console.WriteLine($"{ex.Message}");
+                throw;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                await ReplyAsync($"Quantity and values of dice must be positive (and not above {int.MaxValue}).");
+                Console.WriteLine($"{ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                await ReplyAsync($"An unexpected exception has occurred - who programmed this thing? :skull:");
+                Console.WriteLine($"{ex.Message}");
+                throw;
+            };
         }
 
         [Command("amiadmin")]
